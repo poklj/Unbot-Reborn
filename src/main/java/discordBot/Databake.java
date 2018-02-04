6fec640355aad983b46d7dc128a62de6844b1dac
w0414043 <w0414043@nscc.ca>
2018-02-04 17:58:42 -0400

    Redo MySQL Code

--- src/main/java/discordBot/Databake.java
+++ src/main/java/discordBot/Databake.java
@@ -8,80 +8,50 @@ import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import ch.qos.logback.core.layout.EchoLayout;
 
-public class Databake {
-
-	public boolean IsDBExist(String Filename) {
-		boolean isPresent = false;
+public class Databake  {
+	private String filename = null;
+	private String current_path = "./db/";
+	private String current_file = null;
+	private File constructedPath = null;
+	private File constructedFilePath = null;
+	
+	public Databake(String Filename) {
+		filename = Filename;
+		current_file = "./db/" + filename;
+		
 		try {
-			File tfile = new File("./db/" + Filename).getCanonicalFile();
-			isPresent = tfile.exists();
+			setConstructedPath(new File(current_path).getCanonicalFile());
+			setConstructedFilePath(new File(current_file).getCanonicalFile());
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	
 
-		return isPresent;
+	public File getConstructedPath() {
+		return constructedPath;
+	}
+	
+	public File getConstructedFilePath() {
+		return constructedFilePath;
 	}
 
-	public void CreateDBFile(String Filename) {
-		boolean tfile = false;
-		try {
-			boolean kip = new File("./db/").getCanonicalFile().mkdirs();
-		}catch (Exception e) {
-			e.getStackTrace();
-		}
-		try {
-			
-			tfile = new File("./db/" + Filename).getCanonicalFile().createNewFile();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+	public void setConstructedPath(File constructedPath) {
+		this.constructedPath = constructedPath;
 	}
 
-	public void CreateDB(String Filename) {
-		File tfile = null;
-		try {
-			tfile = new File("./db/").getCanonicalFile();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		String url = ("jdbc:sqlite:" + tfile + Filename);
+	
 
-		try {
-			Connection conn = DriverManager.getConnection(url);
-			if (conn != null) {
-				DatabaseMetaData meta = conn.getMetaData();
-				System.out.println("The driver name is " + meta.getDriverName());
-				System.out.println("A new database has been created.");
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-
+	public void setConstructedFilePath(File constructedFilePath) {
+		this.constructedFilePath = constructedFilePath;
 	}
-	public  void DatabakeE(String Filename) {
-		//load Driver
-		try {
-			Class.forName("org.sqlite.JDBC");
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		// Default Filename
-		if (Filename == null) {
-			Filename = "FallbackDatabake1.db";
-		}
-		
-		if (this.IsDBExist(Filename) != true) {
-			this.CreateDBFile(Filename);
-		}
-		this.CreateDB(Filename);
-	}
+	
 
 }
--- src/main/java/discordBot/MessageListener.java
+++ src/main/java/discordBot/MessageListener.java
@@ -16,10 +16,8 @@ public class MessageListener extends ListenerAdapter {
 		String token = rToken.readToken();
 		JDA jda = new JDABuilder(AccountType.BOT).setToken(token).buildBlocking();
 		jda.addEventListener(new MessageListener());
-		
-		Databake db = new Databake();
-		
-		db.DatabakeE("TestDB.db");
+
+		Databake db = new Databake("testDB.db");
 		
 	}
 
@@ -34,17 +32,9 @@ public class MessageListener extends ListenerAdapter {
 	}
 
 	@Override
-    public void onMessageDelete(MessageDeleteEvent event) {
-    	if (event.isFromType(ChannelType.PRIVATE))
-        {
-//            System.out.printf("[PM] %s: %s\n", event.getAuthor().getName(),
-//                                    event.getMessage().getContentDisplay());
-        }
-        else
-        {
-//            System.out.printf("[%s][%s] %s: %s\n", event.getGuild().getName(),
-//                        event.getTextChannel().getName(), event.getMessageIdLong();
-//            System.out.printf("Content: %s"), ;
-        }
-    }
+	public void onMessageDelete(MessageDeleteEvent event) {
+		if (event.isFromType(ChannelType.PRIVATE)) {
+
+		}
+	}
 }
